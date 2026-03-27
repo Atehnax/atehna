d54793ffdba97fe16661a0553c9b0a9a5eb46175
Atehnax <144073851+Atehnax@users.noreply.github.com>
2026-03-27 23:06:27 +0100

    Redraw trash icon to closely match provided design

--- src/shared/ui/icons/TrashCanIcon.tsx
+++ src/shared/ui/icons/TrashCanIcon.tsx
@@ -6,17 +6,17 @@ export function TrashCanIcon({ className = 'h-[18px] w-[18px]', ...props }: SVGP
       viewBox="0 0 24 24"
       className={className}
       fill="none"
-      stroke="currentColor"
-      strokeWidth="2.35"
+      stroke="#C43A4A"
+      strokeWidth="2.6"
       strokeLinecap="round"
       strokeLinejoin="round"
       aria-hidden="true"
       {...props}
     >
-      <path d="M4.6 7.3h14.8" />
-      <path d="M9.1 7.3V5.8c0-.72.58-1.3 1.3-1.3h3.2c.72 0 1.3.58 1.3 1.3v1.5" />
-      <path d="M7.2 7.5 8 18.8c.08 1.02.93 1.8 1.95 1.8h4.1c1.02 0 1.87-.78 1.95-1.8l.8-11.3" />
-      <path d="M9.8 10.4v6.5M12 10.4v6.5M14.2 10.4v6.5" />
+      <path d="M4.8 7.4h14.4" />
+      <path d="M9.4 7.4V6c0-.77.63-1.4 1.4-1.4h2.4c.77 0 1.4.63 1.4 1.4v1.4" />
+      <path d="M7.3 7.8 8 18.7c.08 1.03.93 1.83 1.96 1.83h4.08c1.03 0 1.88-.8 1.96-1.83l.7-10.9" />
+      <path d="M9.9 10.7v6.1M12 10.7v6.1M14.1 10.7v6.1" />
     </svg>
   );
 }
